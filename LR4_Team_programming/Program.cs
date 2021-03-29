30d6f68ebe70ef1cc102dd40323ed2e099825169
valera1488 <70873029+valera1488@users.noreply.github.com>
2021-03-30 00:43:23 +0700

    Little fix

--- LR4_Team_programming/Program.cs
+++ LR4_Team_programming/Program.cs
@@ -11,18 +11,6 @@ namespace LR4_Team_programming
         [STAThread]
         static void Main()
         {
-            var report = ApiConnector.getReports().ElementAt(0);
-            report.report_lines.RemoveAt(0);
-            var line = new Models.ReportLine();
-            line.detail_pk = 1;
-            line.produced = 100;
-            line.workshop_receiver_pk = 1;
-            report.report_lines.Add(line);
-            report.report_lines.ElementAt(1).produced = 200;
-
-            ApiConnector.editReport(report);
-
-
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
